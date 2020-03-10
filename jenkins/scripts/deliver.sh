11473af0f669cf10ae6b525fb29eb3d647a5c144
rjain236 <raja.jain@finmechanics.com>
2020-03-10 22:35:45 +0800

    fixes permission

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,4 +1,2 @@
 #!/usr/bin/env bash
-#need to be run as default user to grant this service account the cluster admin permission
-kubectl create clusterrolebinding cluster-admin-binding --clusterrole=cluster-admin --user=system:serviceaccount:jenkins-project:default
 echo "delivery is completed!"
--- jenkins/scripts/deploy_dev.sh
+++ jenkins/scripts/deploy_dev.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 echo "Starting deployment"
 kubectl get serviceaccounts
+#need to be run as default user to grant this service account the cluster admin permission
+#kubectl create clusterrolebinding cluster-admin-binding --clusterrole=cluster-admin --user=system:serviceaccount:jenkins-project:default
 kubectl delete ns dev  --grace-period=0 && sleep 0 || true
 kubectl create ns dev
 kubectl --namespace=dev apply -f k8-definition/deployments/
