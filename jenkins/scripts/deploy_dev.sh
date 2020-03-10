90d8705f72f4a6402003be7354fd2e884d400f25
rjain236 <raja.jain@finmechanics.com>
2020-03-10 22:32:07 +0800

    fixes permission

--- jenkins/scripts/deploy_dev.sh
+++ jenkins/scripts/deploy_dev.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 echo "Starting deployment"
 kubectl get serviceaccounts
-kubectl create clusterrolebinding cluster-admin-binding --clusterrole=cluster-admin --user=system:serviceaccount:jenkins-project:default
 kubectl delete ns dev  --grace-period=0 && sleep 0 || true
 kubectl create ns dev
 kubectl --namespace=dev apply -f k8-definition/deployments/
